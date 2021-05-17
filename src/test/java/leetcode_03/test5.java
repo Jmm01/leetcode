662f8648fb698ced91f79b690bd0b9e3637fed25
Jmm01 <1505294785@qq.com>
2021-05-17 22:47:06 +0800

    leetcode练习

--- src/test/java/leetcode_03/test5.java
+++ src/test/java/leetcode_03/test5.java
@@ -1,5 +1,24 @@
 package leetcode_03;
 
 public class test5 {
-
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null)
+            return null;
+        if(head.next == null && head.val == val)
+            return null;
+        ListNode cur = new ListNode();
+        cur.next = head;
+        while(cur.next !=null){
+            if(cur.next.val == val && cur.next == head){
+                head = cur.next.next;
+            }else if(cur.next.val == val){
+                ListNode temp = cur.next;
+                cur.next = cur.next.next;
+                temp.next = null;
+            }else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
 }
